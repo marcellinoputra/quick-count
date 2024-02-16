fcd2e226e80356d4a0d02cef2ce41240e293970b
marcellinoputra <marcellinoputra@github.com>
2024-02-16 16:55:25 +0700

    update multer

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -7,6 +7,7 @@ import { PemilihController } from '../controller/pemilih.controller';
 import PemilihRoutes from './pemilih_routes';
 import { PartaiController } from '../controller/partai.controller';
 import PartaiRoutes from './partai_routes';
+import multer, { diskStorage } from 'multer';
 
 export default function Routes(app: Express) {
   app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,6 +18,42 @@ export default function Routes(app: Express) {
     res.status(200).json({
       message: 'Connected',
     });
+  });
+
+  const upload = multer({
+    storage: multer.diskStorage({
+      destination(req, file, callback) {
+        callback(null, 'images/partai');
+      },
+      filename(req, file, callback) {
+        const getMonth = new Date().getMonth() + 1;
+        const getDay = new Date().getDate();
+        const randomString5Length = Math.random().toString(36).substring(2, 15);
+
+        const getPathExt = file.originalname.split('.')[1];
+        callback(
+          null,
+          file.fieldname +
+            '_' +
+            randomString5Length +
+            '_' +
+            getDay +
+            '' +
+            getMonth +
+            '.' +
+            getPathExt
+        );
+      },
+    }),
+    limits: {
+      fileSize: 1024 * 1024 * 5,
+    },
+    fileFilter(req, file, callback) {
+      if (!file.originalname.match(/\.(jpg|jpeg|png|gif|webp)$/)) {
+        return callback(new Error('Only Image or Webp Files are allowed!'));
+      }
+      callback(null, true);
+    },
   });
 
   const authController = new AuthController();
